a98bf3d8b41789f1058c882ae1dfcec7c860ac75
d58526 <d58526@gmail.com>
2024-10-23 23:55:38 +0800

    feat: Currencies Table Design and Update README.md

---
+++ CoinDeskAPI/CoinDesk.Infrastructure/CurrencyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CoinDesk.Infrastructure.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoinDesk.Infrastructure;
+
+public class CurrencyDbContext : DbContext
+{
+    public CurrencyDbContext(DbContextOptions<CurrencyDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Currency> Currency { get; set; }
+}
